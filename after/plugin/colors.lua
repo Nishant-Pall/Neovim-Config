b35c448544c18bd5b5e657711872d98b74391a20
Nishant Pall <palln6935@gmail.com>
2023-04-28 13:54:16 +0530

    Setup basic init.lua config

---
+++ after/plugin/colors.lua
@@ -0,0 +1,6 @@
+function ColorMyPencils(color)
+	color = color or "gruvbox"
+
+	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+end
---
+++ lua/prhyme/init.lua
@@ -0,0 +1,6 @@
+require("prhyme.remap")
+
+
+vim.cmd[[let g:gruvbox_contrast_dark = 'hard']]
+vim.cmd.colorscheme("gruvbox")
+
