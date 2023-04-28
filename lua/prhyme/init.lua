1a85758d5b0c7e6246adbf3660ee675d6a504bd1
Nishant Pall <palln6935@gmail.com>
2023-04-28 16:13:48 +0530

    Set remaps

--- lua/prhyme/init.lua
+++ lua/prhyme/init.lua
@@ -1,6 +1,8 @@
 require("prhyme.remap")
+require("prhyme.set")
 
 
 vim.cmd[[let g:gruvbox_contrast_dark = 'hard']]
 vim.cmd.colorscheme("gruvbox")
+vim.wo.relativenumber = true
 
--- lua/prhyme/packer.lua
+++ lua/prhyme/packer.lua
@@ -13,6 +13,28 @@ return require('packer').startup(function(use)
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 	use 'morhetz/gruvbox'
+    use 'tpope/vim-fugitive'
 	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+	use 'nvim-treesitter/playground'
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{                                      -- Optional
+			'williamboman/mason.nvim',
+			run = function()
+				pcall(vim.cmd, 'MasonUpdate')
+			end,
+		},
+		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		-- Autocompletion
+		{'hrsh7th/nvim-cmp'},     -- Required
+		{'hrsh7th/cmp-nvim-lsp'}, -- Required
+		{'L3MON4D3/LuaSnip'},     -- Required
+	}
+}
 
 end)
